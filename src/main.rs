4375964f1e4269f7e138fccea193b3ffaf42979f
harrien <mengyeming@outlook.com>
2024-04-22 18:31:40 +0100

    core function finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Just test!");
+use clap::Parser;
+use rcli::{CmdExector, Opts};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let opts = Opts::parse();
+    opts.cmd.execute().await?;
+    Ok(())
 }
---
+++ src/process/gen_pass.rs
@@ -0,0 +1,48 @@
+use std::vec;
+
+use rand::seq::SliceRandom;
+
+const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
+const LOWER: &[u8] = b"abcdefghijkmnopqrstuvwxyz";
+const NUMBER: &[u8] = b"123456789";
+const SYMBOL: &[u8] = b"!@#$%^&*_";
+
+pub fn process_genpass(
+    length: u8,
+    upper: bool,
+    lower: bool,
+    number: bool,
+    symbol: bool,
+) -> anyhow::Result<String> {
+    let mut rng = rand::thread_rng();
+    let mut password = vec![];
+    let mut chars = vec![];
+
+    if upper {
+        chars.extend_from_slice(UPPER);
+        password.push(*UPPER.choose(&mut rng).expect("UPPER won't be empyt"));
+    }
+    if lower {
+        chars.extend_from_slice(LOWER);
+        password.push(*LOWER.choose(&mut rng).expect("LOWER won't be empyt"));
+    }
+    if number {
+        chars.extend_from_slice(NUMBER);
+        password.push(*NUMBER.choose(&mut rng).expect("NUMBER won't be empyt"));
+    }
+    if symbol {
+        chars.extend_from_slice(SYMBOL);
+        password.push(*SYMBOL.choose(&mut rng).expect("SYMBOL won't be empyt"));
+    }
+
+    for _ in 0..(length - password.len() as u8) {
+        let c = chars
+            .choose(&mut rng)
+            .expect("chars won't be empty in this context");
+        password.push(*c);
+    }
+
+    password.shuffle(&mut rng);
+
+    Ok(String::from_utf8(password)?)
+}
